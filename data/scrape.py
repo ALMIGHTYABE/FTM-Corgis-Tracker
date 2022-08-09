1ddaf67e437dff07d08a7beba9c7602c674569b2
ALMIGHTY ABE <5318008bb@gmail.com>
2022-08-09 19:31:15 +0530

    Scrape Updated

--- data/scrape.py
+++ data/scrape.py
@@ -1,7 +1,7 @@
 # Import Packages
-import yaml
-import requests
 import pandas as pd
+import requests
+import yaml
 
 params_path = "./params.yaml"
 
@@ -44,5 +44,71 @@ except Exception as e:
 # Save to CSV
 try:
     corgis_df.to_csv("data/corgis.csv", index=False)
+except Exception as e:
+    error = {"error": e}
+
+# Data Manipulation
+try:
+    sides_batch_wise = pd.DataFrame()
+    sides_list = []
+    max_batch = corgis_df['batch'].value_counts().count()
+    for addy in corgis_df["address"].unique():
+        sides_df = corgis_df[corgis_df["address"] == addy]
+
+        # Sides per Batch
+        batch_counts = pd.DataFrame(sides_df['batch'].dropna().value_counts())
+        batch_counts.reset_index(inplace=True)
+        batch_counts.columns = ['Batch', 'Number of Sides']
+        batch_counts.sort_values(by=['Batch'], axis=0, inplace=True)
+        min_sides = batch_counts['Number of Sides'].min()
+        batch_counts['Bool'] = batch_counts['Number of Sides'].apply(lambda i: True if i >= min_sides else False)
+        batch_counts['address'] = addy
+
+        # Multiplier
+        if (batch_counts["Bool"].all(axis=0)) & (min_sides >= 1) & (len(batch_counts) == max_batch):
+            sides_list.append([addy, min_sides])
+
+        batch_counts = batch_counts[["address", "Batch", "Number of Sides"]]
+        sides_batch_wise = pd.concat([sides_batch_wise, batch_counts], axis=0)
+
+    sides_batch_wise.to_csv("data/Sides Batch Wise.csv", index=False)
+
+    multiplier = pd.DataFrame(sides_list)
+    multiplier.columns = ["address", "Multiplier"]
+    multiplier = multiplier.sort_values(by="Multiplier", ascending=False)
+    multiplier.to_csv("data/Multiplier Address Wise.csv", index=False)
+
+    # Ticket Calculation
+    tickets = 0
+    tickets_list = []
+    for addy in corgis_df["address"].unique():
+
+        # Address Filter
+        filtered = corgis_df[corgis_df["address"] == addy]
+
+        # Main Tickets
+        main = filtered[filtered["type"] == "main"].shape[0] * 4
+
+        # Side Tickets
+        if filtered["address"].isin(multiplier["address"]).all():
+            side = ((multiplier.loc[multiplier["address"] == addy, "Multiplier"].values[0] * max_batch * 2) +
+                    (filtered.loc[filtered["type"] == "side"].shape[0] -
+                     multiplier.loc[multiplier["address"] == addy, "Multiplier"].values[0] * max_batch))
+        else:
+            side = filtered[filtered["type"] == "side"].shape[0]
+
+        # Special Tickets
+        special = filtered[filtered["type"] == "special"].shape[0]
+
+        # Total Tickets
+        tickets = main + side + special
+
+        tickets_list.append([addy, tickets])
+
+    tickets_df = pd.DataFrame(tickets_list)
+    tickets_df.columns = ["address", "tickets"]
+    tickets_df = tickets_df.sort_values(by="tickets", ascending=False)
+    tickets_df.to_csv("data/Tickets Address Wise.csv", index=False)
+
 except Exception as e:
     error = {"error": e}
